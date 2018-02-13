f10bd41b72e3e66f2e191c76ede83881b2fb9b50
kdthomas7 <34221736+kdthomas7@users.noreply.github.com>
2018-02-13 18:38:24 -0500

    Add files via upload
    The code works with this online circuit: https://www.tinkercad.com/things/aGUTaFVYuWw-sumobot-circuit-for-code/editel?sharecode=dgyg3_kNPW8owjCW1XZC0sIfUbvMFuXle8slRrx2JEQ=

---
+++ sumbot_v2.ino
@@ -0,0 +1,45 @@
+const int leftForward = 2;
+const int leftBackward = 3;
+const int rightForward = 4;
+const int rightBackward = 5;
+
+void setup() 
+{
+  pinMode(leftForward , OUTPUT);
+  pinMode(leftBackward , OUTPUT);
+  pinMode(rightForward , OUTPUT);
+  pinMode(rightBackward , OUTPUT);
+
+}
+
+void loop()
+{
+  forward();
+  //stop(3000);
+  //back();
+}
+
+void forward()
+{
+ digitalWrite(leftForward , HIGH);
+ digitalWrite(leftBackward , LOW);
+ digitalWrite(rightForward , HIGH);
+ digitalWrite(rightBackward , LOW); 
+}
+
+void back()
+{
+ digitalWrite(leftForward , LOW);
+ digitalWrite(leftBackward , HIGH);
+ digitalWrite(rightForward , LOW);
+ digitalWrite(rightBackward , HIGH); 
+}
+
+void stop(int timeDel)
+{
+ digitalWrite(leftForward , LOW);
+ digitalWrite(leftBackward , LOW);
+ digitalWrite(rightForward , LOW);
+ digitalWrite(rightBackward , LOW); 
+  delay(timeDel);
+}
